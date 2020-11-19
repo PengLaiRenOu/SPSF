1fa40c341e0582f75f42932ebd8ae5ca79a5f452
DESKTOP-IJ9E2P7\user <s107213034@mail1.ncnu.edu.tw>
2020-11-19 13:47:13 +0800

    'form's action not finish'

--- login.php
+++ login.php
@@ -1,2 +1,18 @@
-<?php
-?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>登入</title>
+</head>
+<body>
+<h1>登入輔助學生經費網站</h1>
+<form method="post" action="">
+
+      Account  : <input name="account" type="text" id="account" /> <br>
+
+      Password: <input name="password" type="password" id="password" /> <br>
+	
+      <input type="submit" name="submit" value="送出" />
+</form>
+</body>
+</html>
