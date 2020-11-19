1ec0db40dc884e9a4d658b62b070033f271f9a5a
DESKTOP-IJ9E2P7\user <s107213034@mail1.ncnu.edu.tw>
2020-11-19 15:07:33 +0800

    15:07

--- path.php
+++ path.php
@@ -1,5 +1,7 @@
 <?php
-  require("model/user.php")
+  require("model/user.php");
+  echo $_POST['account'];
+  echo $_POST['password'];
   $user = User_login($_POST['account'], $_POST['password']);
   if($user){
     header("Location:todoAddForm.php");
