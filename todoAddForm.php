96be5e52dcd4c3741b107c76b84ab32b9b828d0b
LAPTOP-VII2Q2S8\楊筱彤 <s107213017@mail1.ncnu.edu.tw>
2020-11-19 14:54:59 +0800

    student

--- todoAddForm.php
+++ todoAddForm.php
@@ -1,8 +1,12 @@
+<?php
+session_start();
+require("dbconnect.php");
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>學生補助經費申請表</title>
+<title>學生申請表</title>
 <style type="text/css">
 input{
       width: 80px;
